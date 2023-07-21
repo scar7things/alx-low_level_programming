42a55ebfbbf2910471863919c77622e621b46d0f
scar7things <scar7things@gmail.om>
2023-07-21 22:47:04 +0300

    updated numbers for betty style

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - checks for checks for a digit (0 through 9).
  *
@@ -7,11 +6,11 @@
  */
 void print_numbers(void)
 {
- int c;
+int c;
 
- for (c = 48; c < 58; c++)
- {
- _putchar(c);
- }
- _putchar('\n');
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }

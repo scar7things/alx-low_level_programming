ca69f15212c8b2a662ce560e14d22909e91f302f
scar7things <scar7things@gmail.com>
2023-08-30 01:23:05 +0300

    HUH

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
@@ -14,8 +14,8 @@ ifndef LISTS_H
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+ int n;
+ struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#end
+#endif
